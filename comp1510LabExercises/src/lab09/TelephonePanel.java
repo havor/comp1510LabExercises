d2ebe30880ac84b03a3aedee8d7fac6687925397
ryantanhh <tanhaihuacn@hotmail.com>
2016-03-24 21:11:25 -0700

    optimize telephone

--- comp1510LabExercises/src/lab09/TelephonePanel.java
+++ comp1510LabExercises/src/lab09/TelephonePanel.java
@@ -7,16 +7,25 @@ package lab09;
 //Illustrates use of BorderLayout and GridLayout.
 //************************************************************
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 public class TelephonePanel extends JPanel
 {
-public TelephonePanel()
-{
+    String dialedNumber;
+    static final int maxNumLength = 18;
+    JLabel title;
+    PhoneButton dialButton;
+    PhoneButton hangButton;
+    PhoneButton delButton;
+public TelephonePanel() {   
+    dialedNumber = "";
 //set BorderLayout for this panel
     setLayout(new BorderLayout());
     
 //create a JLabel with "Your Telephone" title
-    JLabel title = new JLabel("Your Telephone");
+    title = new JLabel("Your Telephone");
     
 //add title label to north of this panel
     JPanel titlePane = new JPanel();
@@ -28,22 +37,81 @@ public TelephonePanel()
     
     
 //add buttons representing keys to key panel
-   
-    keyPane.add(new JButton("1"));
-    keyPane.add(new JButton("2"));
-    keyPane.add(new JButton("3"));
-    keyPane.add(new JButton("4"));
-    keyPane.add(new JButton("5"));
-    keyPane.add(new JButton("6"));
-    keyPane.add(new JButton("7"));
-    keyPane.add(new JButton("8"));
-    keyPane.add(new JButton("9"));
-    keyPane.add(new JButton("*"));
-    keyPane.add(new JButton("0"));
-    keyPane.add(new JButton("#"));
+    for (int i =1; i <= 9; i++){
+      keyPane.add(new PhoneButton(new Integer(i).toString()));  
+    }
+    
+//    keyPane.add(new JButton("2"));
+//    keyPane.add(new JButton("3"));
+//    keyPane.add(new JButton("4"));
+//    keyPane.add(new JButton("5"));
+//    keyPane.add(new JButton("6"));
+//    keyPane.add(new JButton("7"));
+//    keyPane.add(new JButton("8"));
+//    keyPane.add(new JButton("9"));
+    keyPane.add(new PhoneButton("*"));
+    keyPane.add(new PhoneButton("0"));
+    keyPane.add(new PhoneButton("#"));
 
       
 //add key panel to center of this panel
     add(keyPane, BorderLayout.CENTER);
+    
+    JPanel controlPane = new JPanel();
+    controlPane.setLayout(new GridLayout(1,3,5,10));
+    dialButton = new PhoneButton("Dial");
+    controlPane.add(dialButton);
+    
+    hangButton = new PhoneButton("Hang");
+    controlPane.add(hangButton);
+    
+    delButton = new PhoneButton("Del");
+    controlPane.add(delButton);
+    
+    add(controlPane, BorderLayout.SOUTH);
+}
+
+class PhoneButton extends JButton {
+    PhoneButton(String number){
+        super(number);
+        this.addActionListener(new DialListener());
+    }
+    
+    class DialListener implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        // TODO Auto-generated method stub
+        String keyContent = ((JButton)event.getSource()).getText();
+        switch (keyContent){
+        case "Dial":
+            if(dialedNumber.length() >0 && !dialedNumber.equals(" ")) {
+              title.setText(dialedNumber + " ...");  
+              delButton.setEnabled(false);
+            }            
+            break;
+        case "Hang":
+            dialedNumber = "";
+            title.setText(" ");
+            delButton.setEnabled(true);
+            break;
+        case "Del":
+            int length = dialedNumber.length();
+            if (length >0) {
+                dialedNumber = dialedNumber.substring(0, length-1);
+            } 
+            if (dialedNumber.length() == 0){
+                dialedNumber = " ";
+            }
+            
+            title.setText(dialedNumber);
+            break;
+        default:            
+            if (dialedNumber.length() < maxNumLength)
+                dialedNumber += ((JButton)event.getSource()).getText();
+                title.setText(dialedNumber);
+            break;    
+            }   
+        }
+    } 
 }
 }
